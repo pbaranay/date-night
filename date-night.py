c659605f07e06538821c631709fa77b3eb7018d4
Paul Baranay <pbaranay@gmail.com>
2015-01-31 15:46:53 -0500

    Use Bootstrap (new style).

--- date-night.py
+++ date-night.py
@@ -1,7 +1,10 @@
+# coding=utf-8
+
 from flask import Flask
 from flask import render_template
 from flask import request
 import dill
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -12,21 +15,35 @@ lookup_id = dill.load(open("static/lookup_id.dill"))
 average_similarity = dill.load(open("static/average_similarity.dill"))
 svd = dill.load(open("static/svd.dill"))
 
-
 @app.route('/')
 def index():
+    return render_template('bootstrap_test.html')
+
+@app.route('/old')
+def old_index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def index_post_form():
-    a = request.form['movie-a']
-    b = request.form['movie-b']
-    recos = average_similarity(a, b)
-    return render_template('recommendations.html', a=a, b=b, recos=recos)
+    a = request.form['yours']
+    b = request.form['theirs']
+    try:
+        recos = average_similarity(a, b)
+        recos = [x.title() for x in recos]
+    except:
+        recos = ["Error - Sorry! Please try again."]
+        # TODO: add nicer error page
+    return render_template('bootstrap_recommendations.html', yours=a, theirs=b, recos=recos[0:5])
+#    return render_template('recommendations.html', a=a, b=b, recos=recos)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('bootstrap_about.html')
+
+@app.route('/typeahead')
+def typeahead():
+    return render_template('the-basics.html')
 
 if __name__ == '__main__':
+    Bootstrap(app)
     app.run(host='0.0.0.0', debug=True)
