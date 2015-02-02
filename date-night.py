d622a574cade5513340356891757c03295f26875
Paul Baranay <pbaranay@gmail.com>
2015-02-02 11:41:41 -0500

    Fix global variable issues.

--- date-night.py
+++ date-night.py
@@ -5,14 +5,15 @@ from flask import render_template
 from flask import request
 import dill
 from flask_bootstrap import Bootstrap
-from os import path
+import os
+import sys
 
 app = Flask(__name__)
 
-d = dill.load(open("static/d.dill"))
-inv_d = dill.load(open("static/inv_d.dill"))
-lookup_name = dill.load(open("static/lookup_name.dill"))
-lookup_id = dill.load(open("static/lookup_id.dill"))
+#d = dill.load(open("static/d.dill"))
+#inv_d = dill.load(open("static/inv_d.dill"))
+#lookup_name = dill.load(open("static/lookup_name.dill"))
+#lookup_id = dill.load(open("static/lookup_id.dill"))
 #average_similarity = dill.load(open("static/average_similarity.dill"))
 #svd = dill.load(open("static/svd.dill"))
 
@@ -28,22 +29,31 @@ def old_index():
 def index_post_form():
     a = request.form['yours']
     b = request.form['theirs']
+    d = dill.load(open("static/d.dill"))
+    inv_d = dill.load(open("static/inv_d.dill"))
     try:
-        id_a = lookup_id(a.lower())
-        id_b = lookup_id(b.lower())
-    except:
+        id_a = inv_d[a.lower()]  # lookup_id
+        id_b = inv_d[b.lower()]  # lookup_id
+    except Exception as e:
+#        print >> sys.stderr, e
         recos = ["Error - Sorry! Please try again. (Error finding movie ID's.)"]
         # first argument has to be lower than second argument
     try:
         if id_a < id_b:
-            recos = dill.load(open(path.join("static/","recommendations",str(id_a), str(id_b))))
+            recs_filepath = "static/recommendations/"+str(id_a)+"/"+str(id_b)
+#            recs_filepath = os.path.join("static","recommendations",str(id_a), str(id_b))
+            recos = dill.load(open(recs_filepath))
+#            recos = dill.load(open("static/recommendations/"+str(id_a)+"/"+str(id_b)))
         elif id_a > id_b:
-            recos = dill.load(open(path.join("static/","recommendations",str(id_b), str(id_a))))
+            recs_filepath = "static/recommendations/"+str(id_b)+"/"+str(id_a)
+            recos = dill.load(open(recs_filepath))
+#            recos = dill.load(open("static/recommendations/"+str(id_b)+"/"+str(id_a)))
         elif id_a == id_b:
             recos = ["Error - Please give me two different movies!"]
         recos = [x.title() for x in recos]
         # TODO: make the title'ing better (handle "of", "the", "Bug'S")
-    except:
+    except Exception as e:
+#        print >> sys.stderr, e
         recos = ["Error - Sorry! Please try again. (Error finding recommendations.)"]
         # TODO: add nicer error page
     return render_template('bootstrap_recommendations.html', yours=a, theirs=b, recos=recos[0:5])
